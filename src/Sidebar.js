340bcef42cb249074a67bc98d37679c9cc7869cc
abdulkhaliq7 <abdikhaliq_7@hotmail.com>
2021-02-17 12:08:48 +0800

    added sidebar header

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import "./Sidebar.css";
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Avatar,  IconButton } from '@material-ui/core';
 
 function Sidebar() {
     return (
         <div className='sidebar'>
-            <h2>Sidebar</h2>
-        </div>
+            <div className="sidebar__header">
+                <Avatar src='https://upload.wikimedia.org/wikipedia/commons/4/4d/Flag_of_Somaliland.svg' />
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+        </div> 
     )
 }
 
