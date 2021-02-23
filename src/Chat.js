d67ff7b4d8092da0df520b648a101c0c846af48f
abdulkhaliq7 <abdikhaliq_7@hotmail.com>
2021-02-23 16:47:50 +0800

    added footer to chat

--- src/Chat.js
+++ src/Chat.js
@@ -1,5 +1,5 @@
 import { Avatar, IconButton } from '@material-ui/core';
-import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+import { AttachFile, InsertEmoticon, Mic, MoreVert, SearchOutlined } from '@material-ui/icons';
 import React from 'react';
 import './Chat.css';
 
@@ -35,7 +35,7 @@ function Chat() {
                     </span>
                 </p>
 
-                <p className='chat__message'>
+                <p className='chat__message chat__reciever'>
                     <span className='chat__name'>
                         A7
                     </span>
@@ -44,16 +44,14 @@ function Chat() {
                         {new Date().toUTCString()}
                     </span>
                 </p>
-
-                <p className='chat__message'>
-                    <span className='chat__name'>
-                        A7
-                    </span>
-                    This is a message
-                    <span className='chat__timestamp'>
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
+            </div>
+            <div className="chat__footer">
+                <InsertEmoticon />
+                <form>
+                    <input placeholder='Type a message' type="text"/>
+                    <button type='submit'>Send a message</button>
+                </form>
+                <Mic />
             </div>
         </div>
     )
